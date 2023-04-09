72eb043583c0f9e209491aea4cf397949f2a36fe
Mona Lisa <87797742+Arisa-Kaewsuan@users.noreply.github.com>
2023-04-09 13:59:28 +0700

    add Stylesheet in flatlist

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,19 @@
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import React from "react";
+import NavOption from "../components/NavOption";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="bg-red-900 h-full">
+    <SafeAreaView className="bg-white h-full ">
       {/*<Text style={styles.text}> My First APP Maple ! </Text>*/}
-      <View>
+      <View className="p-5">
         <Image
-          style={{
-            with: 100,
-            height: 100,
-            resizeMode: "contain",
-          }}
-          source={{
-            uri: "https://links.papareact.com/gzs",
-          }}
+          source={{ uri: "https://Links.papareact.com/gzs" }}
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
         />
       </View>
+
+      <NavOption />
     </SafeAreaView>
   );
 };
